9ada89e3477127987dfdfe320c87927b983a8475
龚杰洪 <gjhlovelh@vip.qq.com>
2020-04-02 16:41:39 +0800

    提交

---
+++ ios/Classes/PermissionHandlerEnums.h
@@ -0,0 +1,32 @@
+//
+//  PermissionHandlerEnums.h
+//  permission_handler
+//
+//  Created by Razvan Lung on 15/02/2019.
+//
+
+typedef NS_ENUM(int, PermissionGroup) {
+    PermissionGroupCamera,
+    PermissionGroupLocation,
+    PermissionGroupLocationAlways,
+    PermissionGroupLocationWhenInUse,
+    PermissionGroupMediaLibrary,
+    PermissionGroupMicrophone,
+    PermissionGroupPhotos,
+    PermissionGroupUnknown,
+};
+
+typedef NS_ENUM(int, PermissionStatus) {
+    PermissionStatusDenied = 0,
+    PermissionStatusDisabled,
+    PermissionStatusGranted,
+    PermissionStatusRestricted,
+    PermissionStatusUnknown,
+};
+
+typedef NS_ENUM(int, ServiceStatus) {
+    ServiceStatusDisabled = 0,
+    ServiceStatusEnabled,
+    ServiceStatusNotApplicable,
+    ServiceStatusUnknown,
+};
